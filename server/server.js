92f60ad6e11c289c15524e6cf4aa5acb425f32b1
Aayush Jain <eraayushjain@gmail.com>
2021-11-09 16:41:40 +0530

    Revert Changes

--- server/server.js
+++ server/server.js
@@ -79,14 +79,6 @@ app.use((error, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
-
 mongoose.connect(process.env.MONGO_URL.replace("<username>", process.env.MONGO_USERNAME).replace("<password>", process.env.MONGO_PASSWORD), { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true }).then(() => {
     server.listen(PORT)
     console.log("server Started")
